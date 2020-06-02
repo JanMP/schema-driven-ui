091780ad660b3aee6e5b9d9a1adf5866f7213f7a
JanMP <pille@mac.com>
2020-06-02 10:09:59 +0200

    add queryUiObjectToQuery to package

--- schema-driven-ui.js
+++ schema-driven-ui.js
@@ -1,4 +1,6 @@
+import queryUiObjectToQuery from './imports/ui/QueryEditor/queryUiObjectToQuery'
 export {default as MeteorDataAutoTable} from './imports/ui/AutoTable/MeteorDataAutoTable';
 export {default as createAutoDataTableBackend} from './imports/ui/AutoTable/createAutoDataTableBackend';
 export {default as AppRouter} from './imports/ui/Desktop/AppRouter';
 export {default as QueryEditor} from  './imports/ui/QueryEditor/QueryEditor'
+export {default as queryUiObjectToQuery} from './imports/ui/QueryEditor/queryUiObjectToQuery'
