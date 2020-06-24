611b4104e6a625dd8208c962849e5d28a5a2deb5
JanMP <pille@mac.com>
2020-06-24 16:04:36 +0200

    bump to 0.0.4

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'janmp:schema-driven-ui',
-  version: '0.0.3',
+  version: '0.0.4',
   // Brief, one-line summary of the package.
   summary: 'schema driven data tables, for internal use (for now), no documentation (yet)',
   // URL to the Git repository containing the source code for this package.
